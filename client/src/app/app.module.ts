c9074798c1a302927a17f5e159a35ab4aa3660c1
Neil Halligan <neilhalligan2972@gmail.com>
2017-04-29 12:05:47 +0100

    added new user

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HomeLibraryRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HomeLibraryRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
