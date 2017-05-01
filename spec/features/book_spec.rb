a9f5d5d6e811a433996cc26a891ef22d9d8cc5db
Neil Halligan <neilhalligan2972@gmail.com>
2017-05-01 12:41:39 +0100

    app component test fix

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Books',js: true do
   scenario 'list page' do
     visit '/'
-    expect(page).to have_content('app works!')
+    expect(page).to have_content('Home Library')
   end
 end
